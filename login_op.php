e29348ad60fd28da103145356635d083b35fe061
Elmisa01 <misael15.da@gmail.com>
2025-12-16 10:52:51 -0600

    Quitar botón
    Se quito el botón "Continua carga de documentos obligatorio"

--- login_op.php
+++ login_op.php
@@ -763,7 +763,7 @@ Toda la informaci√≥n que se registre en los apartados de este sitio web, servir√
                   <div class="col-md-8">
                     <div class="form-group clearfix">   
                       <div class="pull-left text-muted text-vertical-align-button">* Campos obligatorios</div>
-                      <div class="pull-right"><input class="btn btn-primary" type="button" value="Continua carga de documentos obligatorios" id="submit1" name="submit1" onClick="validarEnvio();"></div>
+                      <!--div class="pull-right"><input class="btn btn-primary" type="button" value="Continua carga de documentos obligatorios" id="submit1" name="submit1" onClick="validarEnvio();"></div-->
                     </div>
                   </div> 
                 </div>
