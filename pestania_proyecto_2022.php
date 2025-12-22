0979bcb19511eca27f1de26066e283348d488d37
Elmisa01 <misael15.da@gmail.com>
2025-12-22 13:49:42 -0600

    Revert "Cambio de entidades a pestaña"
    This reverts commit cfcc0a4a6bf459ce6ec43c22457b92c03ad11f8e.

--- pestania_proyecto_2022.php
+++ pestania_proyecto_2022.php
@@ -497,7 +497,81 @@
           </div>
           </div>
         </div>
-
+        <!-- inicio Entidad(es) -->
+        <div class="row">
+          <div class="col-md-8"> 
+            <h3>Entidad(es) donde se planea la realización del proyecto</h3>
+          </div>
+            <div class="col-md-12"><hr class="red small-margin"></div>
+        </div>
+        <div class="form-group">
+          <div class="table table-responsive">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Entidad</th>
+                <th>Municipio 1</th>
+                <th>Municipio 2</th>
+                <th>Municipio 3</th>
+                <th>Municipio 4</th>
+                <th>Municipio 5</th>
+                <th>Municipio 6</th>
+              </tr>
+            </thead>
+            <?php for($f=1;$f<=10;$f++){ ?>
+              <tbody>
+                <tr>
+                  <th scope="row">
+                    <?php
+                      echo $f;
+                      $entidades_a=${'entidades_a'.$f};
+                      if($f<=1){ ?>
+                        <samp id="errentidades_a<?=$f?>As" name="errentidades_a<?=$f?>As" class="control-label">*</samp><?php } ?>
+                  </th>
+                  <td> 
+                    <select id="entidades_a<?=$f?>" name="entidades_a<?=$f?>" class="form-control proyectocampo">
+                      <option value="" selected="selected">Selecciona una opción</option>
+                        <?php 
+                            $sql_consulta_ent = "SELECT * FROM `entidades`"; 
+                            $resultado_consulta_ent = mysqli_query($con, $sql_consulta_ent);
+                            $num_resultado_consulta_ent = mysqli_num_rows($resultado_consulta_ent);
+                            for ($m2=0; $m2 <$num_resultado_consulta_ent; $m2++){
+                                $row_proy_ent2 = mysqli_fetch_array($resultado_consulta_ent, MYSQLI_ASSOC);
+                                $id_entidad_proyecto_origen  = $row_proy_ent2["id_entidad_proyecto"];
+                                $nombre_entidad_proyecto  = $row_proy_ent2["nombre_entidad_proyecto"];
+                        ?>
+                      <option value=<?php echo $id_entidad_proyecto_origen; ?> <?php if($id_entidad_proyecto_origen==$entidades_a){ ?> selected="selected" <?php } ?>><?php echo  $nombre_entidad_proyecto; ?></option>
+                    <?php } ?>
+                  </select>
+                  <small id="errentidades_a<?=$f?>" name="errentidades_a<?=$f?>" class="form-text form-text-error" style="display:none">Este campo es obligatorio</small>
+                </td>
+                
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+                <td>
+                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
+                </td>
+              </tr>
+            </tbody>
+          <?php } ?>
+        </table>
+       </div>
+      </div>
+      <!-- fin Entidad(es) -->
 
         <!-- inicio Metas numéricas dentro de Desarrollo del Proyecto dentro de Proyecto -->        
         <div class="row">
@@ -638,82 +712,6 @@
         <!-- fin de la pestaña Desarrollo del Proyecto dentro de Proyecto -->
         </div>
         <!--Inicio de la pestaña Lugar de realización-->
-
-                <!-- inicio Entidad(es) -->
-        <div class="row">
-          <div class="col-md-8"> 
-            <h3>Entidad(es) donde se planea la realización del proyecto</h3>
-          </div>
-            <div class="col-md-12"><hr class="red small-margin"></div>
-        </div>
-        <div class="form-group">
-          <div class="table table-responsive">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Entidad</th>
-                <th>Municipio 1</th>
-                <th>Municipio 2</th>
-                <th>Municipio 3</th>
-                <th>Municipio 4</th>
-                <th>Municipio 5</th>
-                <th>Municipio 6</th>
-              </tr>
-            </thead>
-            <?php for($f=1;$f<=10;$f++){ ?>
-              <tbody>
-                <tr>
-                  <th scope="row">
-                    <?php
-                      echo $f;
-                      $entidades_a=${'entidades_a'.$f};
-                      if($f<=1){ ?>
-                        <samp id="errentidades_a<?=$f?>As" name="errentidades_a<?=$f?>As" class="control-label">*</samp><?php } ?>
-                  </th>
-                  <td> 
-                    <select id="entidades_a<?=$f?>" name="entidades_a<?=$f?>" class="form-control proyectocampo">
-                      <option value="" selected="selected">Selecciona una opción</option>
-                        <?php 
-                            $sql_consulta_ent = "SELECT * FROM `entidades`"; 
-                            $resultado_consulta_ent = mysqli_query($con, $sql_consulta_ent);
-                            $num_resultado_consulta_ent = mysqli_num_rows($resultado_consulta_ent);
-                            for ($m2=0; $m2 <$num_resultado_consulta_ent; $m2++){
-                                $row_proy_ent2 = mysqli_fetch_array($resultado_consulta_ent, MYSQLI_ASSOC);
-                                $id_entidad_proyecto_origen  = $row_proy_ent2["id_entidad_proyecto"];
-                                $nombre_entidad_proyecto  = $row_proy_ent2["nombre_entidad_proyecto"];
-                        ?>
-                      <option value=<?php echo $id_entidad_proyecto_origen; ?> <?php if($id_entidad_proyecto_origen==$entidades_a){ ?> selected="selected" <?php } ?>><?php echo  $nombre_entidad_proyecto; ?></option>
-                    <?php } ?>
-                  </select>
-                  <small id="errentidades_a<?=$f?>" name="errentidades_a<?=$f?>" class="form-text form-text-error" style="display:none">Este campo es obligatorio</small>
-                </td>
-                
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-                <td>
-                  <input class="form-control" placeholder="Municipio" type="Municipio donde se realizara el festival">
-                </td>
-              </tr>
-            </tbody>
-          <?php } ?>
-        </table>
-       </div>
-      </div>
-      <!-- fin Entidad(es) -->
         
       </div><!--fin content-->
     </div>
