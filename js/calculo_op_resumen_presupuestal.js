380f68ce6542bf9a8f6e783faab0e65cefb91083
Jatziry Gonzalez <jghernandez@cultura.gob.mx>
2025-12-26 17:50:32 -0600

    habilitar porcenatje op

--- js/calculo_op_resumen_presupuestal.js
+++ js/calculo_op_resumen_presupuestal.js
@@ -37,7 +37,8 @@ console.log(monto_coinversion2);*/
 								var campov_limpia = document.getElementById('Monto_unidad'+id);
 							    campov_limpia.value = 0;
 								
-								//2025 ya no se ultilizará sumaVerticalporcentajes(id, cuantos)
+								//2025 ya no se ultilizará se habilitara para 2026
+								sumaVerticalporcentajes(id, cuantos);
 								
 								alert('Verifica los montos');
 								}
