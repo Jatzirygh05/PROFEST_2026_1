8c976b8edd59bb4cd7cda7190974e03140d71aed
Jatziry Gonzalez <jghernandez@cultura.gob.mx>
2025-12-27 00:26:38 -0600

    cierre de 3 asteriscos

--- login_op.php
+++ login_op.php
@@ -671,7 +671,7 @@ Toda la informaciÃ³n que se registre en los apartados de este sitio web, servirÃ
                             $Porcentaje=$fila2['Porcentaje'];
                         ?>
                         <td><?php echo $h; 
-                        if($h<=3){ echo "*"; ?><input type="hidden" name="id_presupuesto<?=$h?>" id="id_presupuesto<?=$h?>" value="<?=$id_presupuesto?>">
+                        if($h<=3){ echo "*"; } ?><input type="hidden" name="id_presupuesto<?=$h?>" id="id_presupuesto<?=$h?>" value="<?=$id_presupuesto?>">
                         <input type="hidden" name="cuantos_INSERT" id="cuantos_INSERT" value="<?=$cuantos?>">
                         <td> 
                         <select class='form-control proyectocampo' name='Concepto_gasto<?=$h?>' id='Concepto_gasto<?=$h?>'>
